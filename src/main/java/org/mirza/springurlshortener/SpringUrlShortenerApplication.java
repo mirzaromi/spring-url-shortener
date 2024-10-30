e3bbb3f0a42e5e82f7854789716f71f00689d20a
mirz <mirza.romi@doku.com>
2024-10-30 13:33:20 +0700

    init project

---
+++ src/main/java/org/mirza/springurlshortener/SpringUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package org.mirza.springurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mirza/springurlshortener/SpringUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mirza.springurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
